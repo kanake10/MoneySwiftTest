493c88076d120c03646d0188953eb44d636c70e9
kanake <10kanake@gmail.com>
2024-03-26 03:18:50 +0300

    splash screen

--- app/src/main/java/com/example/moneyswifttest/MainActivity.kt
+++ app/src/main/java/com/example/moneyswifttest/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.moneyswifttest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.moneyswifttest.ui.theme.MoneySwiftTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,33 +13,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoneySwiftTestTheme {
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
+                installSplashScreen()
                 setContent{
                     MainNav()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoneySwiftTestTheme {
-        Greeting("Android")
     }
 }
