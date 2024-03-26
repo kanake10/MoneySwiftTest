727b758f62cea9344e2e3593ccf628f68f226aa4
kanake <10kanake@gmail.com>
2024-03-26 03:27:52 +0300

    remove the apps name from every activity

--- app/src/main/java/com/example/moneyswifttest/MainActivity.kt
+++ app/src/main/java/com/example/moneyswifttest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.moneyswifttest
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState)
         setContent {
             MoneySwiftTestTheme {
